7373c7a2168f6af7a53075b96199e29951a8926e
cookies2002 <cookies2002boy@gmail.com>
2025-05-25 19:46:28 +0530

    Update ping.py

--- RAUSHAN/modules/ping.py
+++ RAUSHAN/modules/ping.py
@@ -1,6 +1,3 @@
-# Don't remove This Line From Here.
-# Telegram :- @ll_ALPHA_BABY_lll
-
 import random
 from datetime import datetime
 
@@ -8,7 +5,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from config import IMG, OWNER_USERNAME, STICKER
+from config import START_IMG_URL, OWNER_USERNAME, STICKER  # Updated here
 from RAUSHAN import BOT_NAME, dev
 from RAUSHAN.database.chats import add_served_chat
 from RAUSHAN.database.users import add_served_user
@@ -19,10 +16,12 @@ from RAUSHAN.modules.helpers import PNG_BTN
 async def ping(_, message: Message):
     await message.reply_sticker(sticker=random.choice(STICKER))
     start = datetime.now()
+    
     loda = await message.reply_photo(
-        photo=random.choice(IMG),
+        photo=START_IMG_URL,
         caption="ᴘɪɴɢ ᴘᴏɴɢ...",
     )
+    
     try:
         await message.delete()
     except:
@@ -30,10 +29,16 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"нєყ вαву!!\n{BOT_NAME} 𝚒ѕ al𝚒ve 🥀 αnd worĸɪng ғɪnє wɪтн ᴀ ᴘɪɴɢ oғ\n➥ `{ms}` ms\n\n<b> мα𝙳є ω𝚒тн ❣️ ву [𝐑𝐄𝐃 - 𝐋𝐈𝐍𝐄 ™](https://t.me/+QuuoMVb6zys0MDA1) </b>",
+        text=(
+            f"нєყ вαву!!\n{BOT_NAME} 𝚒ѕ al𝚒ve 🥀 αnd worĸɪng ғɪnє wɪтн ᴀ ᴘɪɴɢ oғ\n"
+            f"➥ `{ms}` ms\n\n"
+            f"<b> мα𝙳є ω𝚒тн ❣️ ву [𝐑𝐄𝐃 - 𝐋𝐈𝐍𝐄 ™](https://t.me/+QuuoMVb6zys0MDA1) </b>"
+        ),
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
+
     if message.chat.type == ChatType.PRIVATE:
         await add_served_user(message.from_user.id)
     else:
         await add_served_chat(message.chat.id)
+        
