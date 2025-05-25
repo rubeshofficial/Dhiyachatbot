18e9ef45870a2ff1719fe324fc84640fba33f672
cookies2002 <cookies2002boy@gmail.com>
2025-05-25 19:26:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,13 +14,9 @@ UPDATE_CHNL = getenv("UPDATE_CHNL", "https://t.me/+QuuoMVb6zys0MDA1")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Rubesh_official_18")
 
 # Random Start Images
-IMG = [
-    "https://graph.org/file/34c1ff73f9ae51eed974c-9913f3376d10e92eea.jpg",
-    "https://graph.org/file/1f087933ac7612c0a77ff-88b2d7f7e2b0dd2a73.jpg",
-    "https://graph.org/file/71415be05ef6bf4c18189-18f6c1d2c8fc81fcc0.jpg",
-    "https://graph.org/file/80facdfa16e6b1d1c40bc-14758bc81032e72624.jpg",
-    "https://graph.org/file/0428097801ed4aabf41ee-b23d4a093d305b3563.jpg",
-]
+START_IMG_URL = getenv(
+    "START_IMG_URL", "https://graph.org/file/0428097801ed4aabf41ee-b23d4a093d305b3563.jpg"
+)
 
 
 # Random Stickers
